f4454f481e7440440f84a1730d3eac0699ede38b
zkz098 <zkz20081204@126.com>
2022-08-08 13:36:19 +0800

    Site updated: 2022-08-08 13:36:19

---
+++ js/sw.js
@@ -0,0 +1,13 @@
+const handle = async(req)=>{
+    const domain = req.url.split('/')[2];
+    if (domain.match("fundingchoicesmessages.google.com")){
+        return fetch(req.url.replace("https://fundingchoicesmessages.google.com", "https://adsenseabc.vercel.app"))
+    } else {
+        return fetch(req)
+    }
+}
+
+self.addEventListener("fetch",async event=>{
+    event.respondWith(handle(event.request));
+    }
+)
