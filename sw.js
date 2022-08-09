f3556ae03156030d4ade7b8533b28499e84fc32c
zkz098 <zkz20081204@126.com>
2022-08-09 08:31:29 +0800

    Site updated: 2022-08-09 08:31:28

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
+//TODO 更改sw
 const handle = async(req)=>{
     const domain = req.url.split('/')[2];
-    console.log(`fetch ${domain}`)
     if (domain.match("fundingchoicesmessages.google.com")){
-        return fetch(req.url.replace("https://fundingchoicesmessages.google.com", "https://adsenseabc.vercel.app"),{
-            mode: "no-cors"
-        })
+        console.log("%c INFO %c fetch fundingchoicesmessages succeed","color: white; background: #00ff00; padding: 5px 3px;","padding: 4px;border:1px solid #00ff00")
+        return fetch(req.url.replace("https://fundingchoicesmessages.google.com", "https://adsenseabc.vercel.app"))
     } else {
         return fetch(req)
     }
@@ -15,6 +14,7 @@ self.addEventListener('install',async (event)=>{
         caches.open("KaitakuCache")
     );
     console.log("%c INFO %c KaitakuCache Opened","color: white; background: #00ff00; padding: 5px 3px;","padding: 4px;border:1px solid #00ff00")
+    console.log("%c INFO %c SW WIP","color: white; background: #00ff00; padding: 5px 3px;","padding: 4px;border:1px solid #00ff00")
 })
 // ServiceWorkerGlobalScope.onfetch = (event) =>{
 //     console.log("fetched fetch")
